2e69857a9b228533e477b5b1c1d996dd720442b7
Krishandeep Singh <krishandeep17@gmail.com>
2023-08-15 23:42:11 +0530

    add pages

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import AppLayout from "./AppLayout";
+import HomeScreen from "./HomeScreen";
+import Login from "./Login";
+import PageNotFound from "./PageNotFound";
+import Pricing from "./Pricing";
+import Product from "./Product";
+
+export { AppLayout, HomeScreen, Login, PageNotFound, Pricing, Product };

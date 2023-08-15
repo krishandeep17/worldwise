029afa83197020bbfe89724356eb7aa18bed289b
Krishandeep Singh <krishandeep17@gmail.com>
2023-08-15 23:44:29 +0530

    define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { HomeScreen, Login, PageNotFound, Pricing, Product } from "./pages";
+
 const App = () => {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;

2cb5ef9aa17a0f4c7becf82d4aa26ebaf91a6a6f
Krishandeep Singh <krishandeep17@gmail.com>
2023-08-18 14:20:29 +0530

    fetch cities and add nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,73 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { HomeScreen, Login, PageNotFound, Pricing, Product } from "./pages";
+import {
+  AppLayout,
+  Homepage,
+  Login,
+  PageNotFound,
+  Pricing,
+  Product,
+} from "./pages";
 
-const App = () => {
+import { CityList, CountryList } from "./components";
+
+let url;
+
+if (import.meta.env.VITE_ENV === "production") {
+  url = "https://krishandeep17.github.io/data/cities.json";
+} else {
+  url = "http://localhost:8000/cities";
+}
+
+export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true);
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setCities(data);
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        <Route index element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-export default App;
+}

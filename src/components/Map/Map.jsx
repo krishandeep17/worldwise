c075310ed885db01456f5595f02856a69ae5de68
Krishandeep Singh <krishandeep17@gmail.com>
2023-08-18 20:43:04 +0530

    add useNavigate() hook

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,15 +1,17 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 import styles from "./Map.module.css";
 
 export default function Map() {
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h2>
         Position: {lat} {lng}

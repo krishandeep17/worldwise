35d5900105dd00c9719560b0020f3bc6fb240399
Krishandeep Singh <krishandeep17@gmail.com>
2023-09-25 13:10:51 +0530

    create custom scrollbar

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -28,48 +28,50 @@ export default function City() {
   if (isLoading) return <Spinner />;
 
   return (
-    <div className={styles.city}>
-      <div className={styles.row}>
-        <h6>City name</h6>
-        <div className={styles.cityName}>
-          <Flag countryCode={countryCode} />
-          <h3>{cityName}</h3>
-        </div>
-      </div>
-
-      <div className={styles.row}>
-        <h6>You went to {cityName} on</h6>
-        <p>{formatDate(date || null)}</p>
-      </div>
-
-      {notes && (
+    <div className={styles.cityWrapper}>
+      <div className={styles.city}>
         <div className={styles.row}>
-          <h6>Your notes</h6>
-          <p>{notes}</p>
+          <h6>City name</h6>
+          <div className={styles.cityName}>
+            <Flag countryCode={countryCode} />
+            <h3>{cityName}</h3>
+          </div>
         </div>
-      )}
 
-      <div className={styles.row}>
-        <h6>Learn more</h6>
-        <a
-          href={`https://en.wikipedia.org/wiki/${cityName}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check out {cityName} on Wikipedia &rarr;
-        </a>
-      </div>
+        <div className={styles.row}>
+          <h6>You went to {cityName} on</h6>
+          <p>{formatDate(date || null)}</p>
+        </div>
 
-      <div>
-        <Button
-          type="back"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}
-        >
-          &larr; Back
-        </Button>
+        {notes && (
+          <div className={styles.row}>
+            <h6>Your notes</h6>
+            <p>{notes}</p>
+          </div>
+        )}
+
+        <div className={styles.row}>
+          <h6>Learn more</h6>
+          <a
+            href={`https://en.wikipedia.org/wiki/${cityName}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Check out {cityName} on Wikipedia &rarr;
+          </a>
+        </div>
+
+        <div>
+          <Button
+            type="back"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(-1);
+            }}
+          >
+            &larr; Back
+          </Button>
+        </div>
       </div>
     </div>
   );

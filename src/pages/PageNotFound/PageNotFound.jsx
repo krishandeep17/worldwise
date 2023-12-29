efc8668841829a4335e299e0162c4c0f0e0cd9af
Krishandeep Singh <krishandeep17@gmail.com>
2023-12-29 10:50:51 +0530

    add page not found error page

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
+import PageNav from "../../components/PageNav";
+import styles from "./PageNotFound.module.css";
+
 export default function PageNotFound() {
   return (
-    <div>
-      <h1>Page not found 😢</h1>
-    </div>
+    <main className={styles.pageNotFound}>
+      <PageNav />
+      <section>
+        <h2>Sorry, this page isn&apos;t available.</h2>
+        <p>
+          The link you followed may be broken, or the page may have been
+          removed.{" "}
+          <Link to="/" className={styles.link}>
+            Go back to WorldWise
+          </Link>
+        </p>
+      </section>
+    </main>
   );
 }
